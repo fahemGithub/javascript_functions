e70eefd83f5f225722b0bce7827a1fef6374fc89
fahemGithub <fahemeducaton@gmail.com>
2023-11-30 18:21:43 +0100

    top array functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cars = ["Fiat", "Mercedes", "odi", "volkswagen", "suzuki"];
 // pop supprimer drn el # push
 // shift supprimer premier el # unshift
+// splice(2, 0) 2 = la position ou on veut ajouter un element, 0 = combien delement a souprimer
 // cars.push("Cheery");
 // // console.log(cars.join(","));
 // console.log(cars.pop());
@@ -22,6 +23,23 @@ const legumes = ["carrots", "pommes de terre", "tomates"]
 //
 //map\
 
-cars.map((i, index) => {
-    console.log(i, index)
+// cars.map((i, index) => {
+//     console.log(i, index)
+// })
+
+// filter
+const ages = [10, 12, 18, 25, 35]
+const mageur = ages.filter((i, index) => i >= 18)
+// console.log(mageur)
+
+// splice
+
+// cars.splice(2, 1, 'tata', 'jac')
+
+// sort
+
+const numbers = [98, 42, 67, 12, 29, 0, 83, 6]
+const ordered_numbers = numbers.sort((a, b ) => {
+    return b - a;
 })
+console.log(ordered_numbers)
