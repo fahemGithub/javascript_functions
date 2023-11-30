539dc6bcd933fccfa174184c8cb41874b0261e28
fahemGithub <fahemeducaton@gmail.com>
2023-11-30 18:05:30 +0100

    fonctions tableau

---
+++ index.js
@@ -0,0 +1,27 @@
+const cars = ["Fiat", "Mercedes", "odi", "volkswagen", "suzuki"];
+// pop supprimer drn el # push
+// shift supprimer premier el # unshift
+// cars.push("Cheery");
+// // console.log(cars.join(","));
+// console.log(cars.pop());
+// console.log(cars.shift())
+// console.log(cars)
+// console.log(cars.unshift("cheery"))
+// console.log(cars)
+
+
+const fruits = ["bananes", "oranges", "freizes", "sourises"]
+const legumes = ["carrots", "pommes de terre", "tomates"]
+
+// console.log(cars.concat(fruits, legumes))
+
+// foreach,
+// cars.forEach((i, index) => {
+//     console.log(i, index)
+// })
+//
+//map\
+
+cars.map((i, index) => {
+    console.log(i, index)
+})
